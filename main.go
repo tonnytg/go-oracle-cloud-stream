9f44e87fd3cddb1de504aabf2da2186cb93a20c5
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-13 23:33:26 -0300

    feat: add list oci tanancy domains

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/oracle/oci-go-sdk/v49/common"
+	"github.com/oracle/oci-go-sdk/v49/identity"
 )
 
 func main() {
@@ -12,4 +14,28 @@ func main() {
 	configProvider := common.DefaultConfigProvider()
 	fmt.Println(configProvider)
 
+	client, err := identity.NewIdentityClientWithConfigurationProvider(configProvider)
+	if err != nil {
+		fmt.Println("ClientError:", err)
+		return
+	}
+
+	// The OCID of the tenancy containing the compartment.
+	tenancyID, err := common.DefaultConfigProvider().TenancyOCID()
+	if err != nil {
+		fmt.Println("TanancyError:", err)
+		return
+	}
+
+	request := identity.ListAvailabilityDomainsRequest{
+		CompartmentId: &tenancyID,
+	}
+
+	r, err := client.ListAvailabilityDomains(context.Background(), request)
+	if err != nil {
+		fmt.Println("ListDomainsError:", err)
+		return
+	}
+
+	fmt.Printf("List of available domains: %v", r.Items)
 }
